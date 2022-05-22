8250dd6bc21bdaf2cb2eb8b4bb2e85d4b9271ef9
Gustavo Alves <90219607+Gustavollema@users.noreply.github.com>
2022-05-22 13:08:59 -0300

    conectando prisma

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
+import { PrismaModule } from '../prisma/prisma.module';
 @Module({
+  imports: [PrismaModule],
   controllers: [GameController],
   providers: [GameService],
 })
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGameDto } from './dto/create-game-dto';
 import { Game } from './entities/game.entity';
 
 @Injectable()
 export class GameService {
   games: Game[] = [];
+
+  constructor(private readonly prisma: PrismaService) {}
+
   findAll() {
-    return this.games;
+    return this.prisma.game.findMany();
   }
 
   create(createGameDto: CreateGameDto) {
-    const game: Game = { id: 'random_id', ...createGameDto};
-    this.games.push(game);
-
-    return game;
+    const game: Game = { ...createGameDto};
+    return this.prisma.game.create({
+      data: game,
+    })
   }
 }
